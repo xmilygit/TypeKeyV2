6c55e16b642b82d9dba24ee10c8e4db396d421f6
xmilygit <xmilygame@163.com>
2018-11-23 22:45:40 +0800

    增加navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,46 @@
 <template>
   <div id="app">
-    <b-modal id="modal1" title="练习成绩" v-model="modalshow" cancel-title="取消"	ok-title="确定" @ok="gotoReady">
+    <b-navbar toggleable="md" type="dark" variant="info">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+      <b-navbar-brand href="#">键盘指法练习</b-navbar-brand>
+    <b-collapse is-nav id="nav_collapse">
+
+    <b-navbar-nav>
+      <b-nav-item href="#">Link</b-nav-item>
+      <b-nav-item href="#">Disabled</b-nav-item>
+    </b-navbar-nav>
+
+    <!-- Right aligned nav items -->
+    <b-navbar-nav class="ml-auto">
+      <b-nav-item-dropdown text="课程选择" right>
+        <b-dropdown-item href="#">EN</b-dropdown-item>
+        <b-dropdown-item href="#">ES</b-dropdown-item>
+        <b-dropdown-item href="#">RU</b-dropdown-item>
+        <b-dropdown-item href="#">FA</b-dropdown-item>
+      </b-nav-item-dropdown>
+
+  
+      <b-nav-form v-if="!login"> 
+        <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="用户名"/>
+        <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="密码"/>
+        <b-button size="sm" class="my-2 my-sm-0" type="submit">登录</b-button>
+      </b-nav-form>
+      <b-nav-item-dropdown right v-else>
+        <!-- Using button-content slot -->
+        <template slot="button-content">
+          <em>User</em>
+        </template>
+        <b-dropdown-item href="#">Profile</b-dropdown-item>
+        <b-dropdown-item href="#">Signout</b-dropdown-item>
+      </b-nav-item-dropdown>
+    </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+  <b-modal id="modal1" title="练习成绩" v-model="modalshow" cancel-title="取消"	ok-title="确定" @ok="gotoReady">
     <p class="my-4">本次练习综合成绩为：{{result}}</p>
   </b-modal>
-    <b-container>
+    <b-container style="margin-top:30px;">
+      
       <b-row style="padding-bottom:20px;">
         <b-col>
           <b-button variant="success"><i class="material-icons buttonoffset">check</i>正确：{{rightCount}} 个</b-button>
@@ -59,6 +96,7 @@ export default {
       countTime: "开  始",
       modalshow: false,
       result: 0,
+      login:false,
     };
   },
   computed: {
@@ -223,7 +261,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 
 h1,
